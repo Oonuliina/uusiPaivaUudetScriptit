6ecd18c450926e8e7fda893163df6b3b98bfd6d1
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2022-10-27 21:04:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,24 +3,23 @@ let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 var listOfDos = document.createElement('ul');
-    toDoContainer.appendChild(listOfDos);
+    toDoContainer.appendChild(listOfDos); 
+   
 /* var tehty = []; */
 
-addToDoButton.addEventListener('click', function(){
-
+addToDoButton.addEventListener('click', function() {
     var paragraph = document.createElement('li');
     paragraph.innerText = inputField.value;
     var pituus = paragraph.innerText.length;
 
-    if (pituus.lenght<2) {
-        alert("Use more letters to express yourself<3");
+    if (pituus < 2) {
+        window.alert("Use more than one letter to express yourself <3");
+        inputField.style.borderColor = "red";
+    } else {
+        listOfDos.appendChild(paragraph);
+        inputField.value = "";
+        inputField.style.borderColor = ""; 
     }
-
-    listOfDos.appendChild(paragraph);
-
-
-
-    inputField.value = "";
     
     paragraph.addEventListener('click', function() {
         paragraph.style.textDecoration = "line-through";
@@ -28,10 +27,14 @@ addToDoButton.addEventListener('click', function(){
 
     paragraph.addEventListener('dblclick', function() {
         listOfDos.removeChild(paragraph);
-    })
+    }); 
 
+/* 
+    var tekstitys = JSON.stringify(listOfDos);
+    var varasto = localStorage.setItem(bucket, tekstitys);   */
     
 /*     tehty.appendChild(paragraph);
     localStorage.setItem("tehty", JSON.stringify(tehty)); */
 
 });
+
